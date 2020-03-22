768804d6d50f7172165ca2f1ac5e3607ad4224e0
Erin Faulkner <40076702+faulknererin@users.noreply.github.com>
2020-03-22 18:44:41 -0400

    Update models.py

--- canvas_app/models.py
+++ canvas_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class LoginProfessor(models.Model):
+    email = models.CharField(max_length = 100, unique = True)
+    password = models.CharField(max_length = 100)
